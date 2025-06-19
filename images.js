192a89e5779087d5f11e4490d7e0f098938df273
TheCarrotCutter <masonthedecker@gmail.com>
2025-06-19 11:28:15 -0400

    Create images.js

---
+++ images.js
@@ -0,0 +1,25 @@
+// ------------------- //
+// GENERATING TEXTURES //
+// ------------------- //
+
+let imageGenerator;
+
+export function generateTextures(scene) {
+  console.log("Generating Textures...")
+  
+  imageGenerator = scene.add.graphics();
+
+
+  imageGenerator.fillStyle(0xffffff, 1);
+  imageGenerator.fillCircle(20, 20, 20);
+  imageGenerator.generateTexture('imageCircle', 40, 40);
+  imageGenerator.clear();
+            
+  imageGenerator.fillStyle(0xff0000, 1)
+  imageGenerator.fillCircle(20, 20, 20);
+  imageGenerator.fillStyle(0xffffff, 1)
+  imageGenerator.fillCircle(20, 20, 18);
+  imageGenerator.generateTexture('circleRedOutline', 40, 40);
+  imageGenerator.clear();
+  console.log("Textures Succesfully Generated!")
+}
