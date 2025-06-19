19cbb98ac6923dc8a34bd6a17f2309d96e5dc182
TheCarrotCutter <masonthedecker@gmail.com>
2025-06-19 11:28:00 -0400

    Create player.js

---
+++ player.js
@@ -0,0 +1,82 @@
+// ----------------- //
+// IMPORT NECECETIES //
+// ----------------- //
+
+// --------------- //
+// PLAYER CREATION //
+// --------------- //
+
+export let player;
+let circleParticle
+let playerEmitter
+
+export function createPlayer(scene, x, y) {
+  console.log("Player Creating...")
+  
+  player = scene.physics.add.sprite(0, 0, 'imageCircle');
+  player.setDisplaySize(30, 30);
+  player.body.setCircle(20, 0, 0);
+  
+  playerEmitter = scene.add.particles(0, 0, 'imageCircle', {
+    x: 0,
+    y: 0,
+    speed: 0,
+    lifespan: 300,
+    quantity: 1,
+    alpha: { start: 0.75, end: 0 },
+    scale: { start: 0.75, end: 0.1 }
+  });
+
+  playerEmitter.startFollow(player);
+  
+  // ---------------- //
+  // PLAYER COLLISION //
+  // ---------------- //
+  
+  function playerCollide(player, object) {
+            
+    if (enemyPhysicsGroup.contains(object)) {
+      let bounce = 
+        getDirectionVector(
+          new Phaser.Math.Vector2(player.x, player.y),
+          new Phaser.Math.Vector2(object.x, object.y),
+          400
+        )
+            
+      player.body.velocity.x += -bounce.x
+      player.body.velocity.y += -bounce.y
+    }
+  }
+  console.log("Player Succesfully Created!")
+  
+  
+}
+
+let mouseMoveX = 0;
+let mouseMoveY = 0;
+
+export function updatePlayer() {
+  
+  // check if player exists
+  if (player) {
+  
+    player.body.velocity.x += mouseMoveX * 1.25;
+    player.body.velocity.y += mouseMoveY * 1.25;
+
+    player.body.velocity.x *= 0.97;
+    player.body.velocity.y *= 0.97;
+
+    if (Math.abs(player.body.velocity.x) < 10) player.body.velocity.x = 0;
+    if (Math.abs(player.body.velocity.y) < 10) player.body.velocity.y = 0;
+    
+    mouseMoveX = 0;
+    mouseMoveY = 0;
+    
+  }
+}
+
+
+document.addEventListener('mousemove', function(event) {
+  mouseMoveX = event.movementX;
+  mouseMoveY = event.movementY;
+});
